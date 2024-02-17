c055d5f90454c7649a8748b7d5c79b3a7e557e29
yousif <yousifsayer@gmail.com>
2024-02-17 15:57:36 +0300

    initial streamlit mock with servers list functionality

---
+++ frontend/app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
+
+
+def create_form():
+    st.markdown("<h1 style='text-align:center;'>login page</h1>", unsafe_allow_html=True)
+    with st.form("Login"):
+        username = st.text_input("username")
+        password = st.text_input("password", type="password")
+        submit_button = st.form_submit_button("Click")
+        if submit_button:
+            if username and password:
+
+                st.success("Logged in successfully")
+                st.switch_page("pages/servers.py")
+            else:
+                st.warning("Invalid username or password")
+
+create_form()
