8c472aff01f221161c9dfb951aa0a33ce13f766b
yousif <yousifsayer@gmail.com>
2024-03-25 21:02:03 +0300

    Modify admin page

--- frontend/form.py
+++ frontend/form.py
@@ -7,6 +7,7 @@ db = Database()
 
 if 'username' not in st.session_state:
     st.session_state['username'] = None
+    st.session_state['role'] = None
 
 def create_form():
     st.markdown("<h1 style='text-align:center;'>login page</h1>", unsafe_allow_html=True)
@@ -17,6 +18,7 @@ def create_form():
         if submit_button:
             if db.user_login(username,password):
                 st.session_state['username'] = username
+                st.session_state['role'] = 'user'
                 st.success("Logged in successfully")
                 st.switch_page("frontend/pages/admin_v2.py")
                     
--- frontend/pages/admin_v2.py
+++ frontend/pages/admin_v2.py
@@ -5,35 +5,61 @@ from datastore.db import Database
 import paramiko
 from paramiko import SSHClient
 
+import logging
 
 
+def check_permissions_based_on_role(status, role):
+    role_has_permissions = status in ["restart", "stop"] and role not in ["admin", "super-admin"]
+
+    return role_has_permissions
 
 
+def check_permissions_based_on_user(status, username, server):
+
+    # TODO: get value from database table
+
+    role_has_permissions = False
+
+    return role_has_permissions
 
 db = Database()
+
+
 
 server, user, privileges = st.tabs(["Server Managment", "User Managment",'Privileges'])
 
 
 
-def create_dynamic_buttons(idx,status):
+def create_dynamic_buttons(idx,status, role="user"):
     
      for item in range(len(servers)):
          count = idx*len(servers) + item
-         action_button = st.button(status, key=count)
-         
-         if action_button:
-             
-             clinet = SSHClient()
-             clinet.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-             
-             clinet.connect(hostname="",username='',password='')
-             stdin,stdout,stderr = clinet.exec_command(f'sudo -S systemctl {status} {st.session_state["service"]}')
-             stdin.write("" + "\n")
-             stdin.flush()
-             print(f'STDOUT: {stdout.read().decode("utf8")}')
-             print(f'STDERR: {stderr.read().decode("utf8")}') 
-             clinet.close()
+
+         role_has_permissions = check_permissions_based_on_role(role, status)
+
+         if role_has_permissions:
+
+            action_button = st.button(status, key=count)
+
+            
+            if action_button:
+                
+                clinet = SSHClient()
+                clinet.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                
+                clinet.connect(hostname="192.168.100.114",username='yousif',password='Ss@s1598')
+                command_string = f'sudo -S systemctl {status} {st.session_state["service"]}'
+
+                logging.info("Attempting to execute the following command:")
+                logging.info(command_string)
+                
+                stdin,stdout,stderr = clinet.exec_command(command=command_string)
+                stdin.write("Ss@s1598" + "\n")
+                stdin.flush()
+                print(f'STDOUT: {stdout.read().decode("utf8")}')
+                print(f'STDERR: {stderr.read().decode("utf8")}') 
+                clinet.close()
+
 
 def generate_services(idx,service_name):
     for server in range(len(servers)):
@@ -54,16 +80,18 @@ with server:
                 db.insert_into(server_ip,service)
     with listing_servers:
         col1, col2, col3, col4, col5 = st.columns(5)
+
+        role = st.session_state["role"]
         with col1:
             servers = db.list_servers()
             for server in servers:
                 st.markdown(server[0])
         with col2:
-            create_dynamic_buttons(100,'stop')
+            create_dynamic_buttons(100,'stop', role)
         with col3:
-            create_dynamic_buttons(200,'restart')
+            create_dynamic_buttons(200,'restart', role)
         with col4:
-            create_dynamic_buttons(300,'start')
+            create_dynamic_buttons(300,'start', role)
 
         with col5:
             services = db.list_services()
